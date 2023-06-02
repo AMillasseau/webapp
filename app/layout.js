f905d9c215e62af2542f7f603adc0ef1dc836467
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-02 12:58:55 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import 'app/layout.css'
+import { Secular_One } from 'next/font/google'
+
+const font1 = Secular_One({ 
+  subsets : ['latin']
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+    <head>
+    <title> Arena Catalog </title>
+    <meta name="description" content="Free Web tutorials">
+    <meta name="author" content="Adrien Millasseau">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body className={font1.className}>{children}</body>
+    </html>
+  )
+}
