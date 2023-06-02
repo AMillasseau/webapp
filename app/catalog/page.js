8371d9834b0bde9cf1c34d80fecf9882ec07574b
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-02 14:10:01 +0200

    Update page.js

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -1,1 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
 
+import style from 'app/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+  
+      <div className={style.head}>
+        <p> Website of Arena </p>
+        <div>
+          <Image 
+            src="/logo.png"
+            width={500}
+            height={500}
+            alt="Arena's logo"
+          />
+        </div>
+      </div>
+
+      <div className={style.center}>
+        
+        
+        
+      </div>
+
+      <div className={style.tail}>
+        
+      </div>
+    
+      </main>
+    )
+}
