be011fab6ae8777c0b6ceede2cd080a510e55352
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-02 12:11:17 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import style from 'app/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+  
+      <div className={style.head}>
+        
+      </div>
+
+      <div className={style.center}>
+        
+        
+        
+      </div>
+
+      <div className={style.tail}>
+        
+      </div>
+    
+      </main>
+    )
+}
