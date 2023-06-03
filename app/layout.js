a18912807c93fabe43e7ef2814ba03f15417f191
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-03 16:23:59 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <meta name="description" content="Free Web tutorials"/>
       <meta name="author" content="Adrien Millasseau"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <link rel="shortcut icon" href="/favicon.ico"/>
+      <link rel="icon" type="image/png" href="/icon.png"/>
     </head>
     <body className={font1.className}>{children}</body>
     </html>
