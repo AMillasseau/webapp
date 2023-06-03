5d5d21a4323ea57a108d801e5d534326b1f88747
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-03 16:08:41 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,11 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-    <title> Arena Catalog </title>
-    <meta name="description" content="Free Web tutorials"/>
-    <meta name="author" content="Adrien Millasseau"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico"/>
+      <title> Arena Catalog </title>
+      <meta name="description" content="Free Web tutorials"/>
+      <meta name="author" content="Adrien Millasseau"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="icon" type="image/x-icon" href="./favicon.ico"/>
     </head>
     <body className={font1.className}>{children}</body>
     </html>
