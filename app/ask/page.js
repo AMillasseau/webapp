1ea6bfe02022a5e2daf2488a4470ea5f589f784e
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 16:04:51 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -6,7 +6,18 @@ import style from 'app/page.module.css'
 export default function Home() {
   return (
     <div>
-    <script type="text/javascript" src="pages/api/examples.js"></script>
+    <script type="text/javascript" src="pages/api/examples.js">
+    function addToCSV() {
+        var string1 = document.getElementById('s1').value;
+        var string2 = document.getElementById('s2').value;
+        var string3 = document.getElementById('s3').value;
+        string3.replace(/,/g, ' ');
+        const data = string1 + ',' + string2 + ',' + string3 + '\n';
+        fs.writeFile('data.csv','name,email,message', 'utf8', (err) => (null));
+        fs.appendFile('data.csv', data, (err) => (null));
+    }
+    
+    </script>
 
     <main className={style.main}>
   
@@ -36,13 +47,12 @@ export default function Home() {
             />
             </a>
           </div>
-        <form onsubmit="addToCSV(this.name.value, this.contact.value, this.text.value);">
-          <input className={style.ipt} name="name" type="text" placeholder="Name" autocomplete="off" required/>
-          <input className={style.ipt} name="contact" type="email" placeholder="Email contact"/>
-          <textarea className={style.txta} name="text" placeholder="Your message" autocomplete="off" rows="12" required></textarea>
-          <button className={style.btn} type="submit"> Submit </button>
-        </form>
-        <button className={style.btn} onclick="addToCSV('connard', 'connard@asshole.com', 'connarsse');"> Test </button>
+          <input className={style.ipt} id="s1" name="name" type="text" placeholder="Name" autocomplete="off" required/>
+          <input className={style.ipt} id="s2" name="contact" type="email" placeholder="Email contact"/>
+          <textarea className={style.txta} id="s3" name="text" placeholder="Your message" autocomplete="off" rows="12" required></textarea>
+          <button className={style.btn} onclick="addToCSV();"> Submit </button>
+        
+        <button className={style.btn} > Test </button>
       </div>
 
       <div className={style.tail}>
