215bc0f4a45fe5386243d785b34fb3997c16b79d
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 16:30:34 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -6,11 +6,31 @@ import style from 'app/page.module.css'
 export default function Home() {
   return (
     <div>
-    <script type="text/javascript" src="pages/api/examples.js">
-    
-    
-    </script>
+    <script type="text/javascript">
+    async function createCSV() {
+      const string1 = document.getElementById('s1').value;
+      const string2 = document.getElementById('s2').value;
+      const string3 = document.getElementById('s3').value;
 
+      try {
+        const response = await fetch('/api/create-csv', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ string1, string2, string3 })
+        });
+
+        if (response.ok) {
+          console.log('CSV file created successfully.');
+        } else {
+          console.error('Failed to create CSV file.');
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    }
+  </script>
     <main className={style.main}>
   
       <div className={style.head}>
