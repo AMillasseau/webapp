19512403db34c3bd6e9d7955762131c6b0f268b1
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 17:17:11 +0200

    Update save.php

--- app/ask/save.php
+++ app/ask/save.php
@@ -1,29 +1,8 @@
 <?php
- $fieldA = $_POST["textnames"];
- $fieldB = $_POST["fathername"];
- $fieldC = $_POST["paddress"];
- $fieldD = $_POST["personaladdress"];
- $fieldE = $_POST["sex"];
- $fieldF = $_POST["Stream"];
- $fieldG = $_POST["Core-1"];
- $fieldH = $_POST["Core-2"];
- $fieldI = $_POST["Elective-1"];
- $fieldJ = $_POST["Elective-2"];
- $fieldK = $_POST["Elective-3"];
- $fieldL = $_POST["Elective-4"];
- $fieldM = $_POST["Compulsory"];
+$ar = [$_GET["name"],$_GET["contat"],$_GET["message"] 
 
- $keys = array($fieldA,$fieldB,$fieldC,$fieldD,$fieldE,$fieldF,$fieldG,$fieldH,$fieldI,$fieldJ,$fieldK,$fieldL,$fieldM); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
- $csv_line = $keys;
- foreach( $keys as $key ){
-     array_push($csv_line,'' . $_GET[$key]);
-
- }
  $fname = 'data.csv'; //NAME OF THE FILE
- $csv_line = implode(',',$csv_line);
- if(!file_exists($fname)){$csv_line = $csv_line."\r\n" ;}
  $fcon = fopen($fname,'a');
- $fcontent = $csv_line;
- fwrite($fcon,$csv_line);
+ $fputcsv($con,$ar);
  fclose($fcon);
  ?>
