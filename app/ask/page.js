92577d135cd5544d97096d85bcdbf7d65839ee62
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 16:39:16 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -5,9 +5,32 @@ import React from 'react';
 import style from 'app/page.module.css'
 
 export default function Home() {
+  const createCSV = async () => {
+    const string1 = document.getElementById('s1').value;
+    const string2 = document.getElementById('s2').value;
+    const string3 = document.getElementById('s3').value;
+
+    try {
+      const response = await fetch('/api/create-csv', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ string1, string2, string3 })
+      });
+
+      if (response.ok) {
+        console.log('CSV file created successfully.');
+      } else {
+        console.error('Failed to create CSV file.');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
   return (
     <div>
-    <script src="/scripts/create-csv.js" />
     <main className={style.main}>
   
       <div className={style.head}>
