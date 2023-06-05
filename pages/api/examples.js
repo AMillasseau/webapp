67f027194d272044b46a4ab0dfea47ffe06f33f7
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 15:38:27 +0200

    Update examples.js

--- pages/api/examples.js
+++ pages/api/examples.js
@@ -5,3 +5,4 @@ function addToCSV(string1, string2, string3) {
   string3.replace(/,/g, ' ');
   const data = string1 + ',' + string2 + ',' + string3 + '\n';
   fs.appendFile('data.csv', data, (err) => (null));
+}
