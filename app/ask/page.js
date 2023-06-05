44776f8b177525af9f95e2571f1db1b27428e335
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 16:44:26 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -5,7 +5,7 @@ import React from 'react';
 import style from 'app/page.module.css'
 
 export default function Home() {
-  function createCSV() {
+  async function createCSV() {
     const string1 = document.getElementById('s1').value;
     const string2 = document.getElementById('s2').value;
     const string3 = document.getElementById('s3').value;
