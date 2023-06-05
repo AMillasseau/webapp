55e6ccf2c9d6c50c7e4d25618fddaefcb913f152
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 16:06:07 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -7,15 +7,7 @@ export default function Home() {
   return (
     <div>
     <script type="text/javascript" src="pages/api/examples.js">
-    function addToCSV() {
-        var string1 = document.getElementById('s1').value;
-        var string2 = document.getElementById('s2').value;
-        var string3 = document.getElementById('s3').value;
-        string3.replace(/,/g, ' ');
-        const data = string1 + ',' + string2 + ',' + string3 + '\n';
-        fs.writeFile('data.csv','name,email,message', 'utf8', (err) => (null));
-        fs.appendFile('data.csv', data, (err) => (null));
-    }
+    
     
     </script>
 
