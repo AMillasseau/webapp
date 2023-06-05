34c637d326a027f5fdd4741c81b26d8c4233b8a9
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 13:26:16 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -22,7 +22,7 @@ export default function Home() {
       </div>
 
       <div className={style.center}>
-        <div style="padding-bottom: 10px" className={style.backarrow}>
+        <div style={{paddingRight: '10px'}} className={style.backarrow}>
           <a href="../">
           <Image 
             src="/backarrow.png"
