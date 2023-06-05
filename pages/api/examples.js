76374fce454bebf58fedbba7937c060aa4d469dd
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 15:40:38 +0200

    Update examples.js

--- pages/api/examples.js
+++ pages/api/examples.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 
-const fs = require('fs');
 function addToCSV(string1, string2, string3) {
   string3.replace(/,/g, ' ');
   const data = string1 + ',' + string2 + ',' + string3 + '\n';
