9151104526b0b3b2d4cd69d2ae3d11df74369004
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-05 16:06:14 +0200

    Update examples.js

--- pages/api/examples.js
+++ pages/api/examples.js
@@ -1,8 +1,11 @@
 import fs from 'fs';
 
-function addToCSV(string1, string2, string3) {
-  string3.replace(/,/g, ' ');
-  const data = string1 + ',' + string2 + ',' + string3 + '\n';
-  fs.writeFile('data.csv','name,email,message', 'utf8', (err) => (null));
-  fs.appendFile('data.csv', data, (err) => (null));
+function addToCSV() {
+        var string1 = document.getElementById('s1').value;
+        var string2 = document.getElementById('s2').value;
+        var string3 = document.getElementById('s3').value;
+        string3.replace(/,/g, ' ');
+        const data = string1 + ',' + string2 + ',' + string3 + '\n';
+        fs.writeFile('data.csv','name,email,message', 'utf8', (err) => (null));
+        fs.appendFile('data.csv', data, (err) => (null));
 }
