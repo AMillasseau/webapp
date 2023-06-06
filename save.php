651cea4c8dcfe8ea2e5eb61fcd481f9da83d98e1
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-06 14:16:24 +0200

    Update and rename public/scripts/save.php to save.php

--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
 // Check if form is submitted and we have the fields we want.
-	$file = "data.txt";
+	$file = "./data.txt";
 	$text =  $_POST["name"];
 	// This file will create a data.txt file and put whatever is in the POST field mytext into the text and put a new line on the end.
 	// The FILE_APPEND allows you to append text to the file. LOCK_EX prevents anyone else from writing to the file at the same time.
