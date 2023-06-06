1d96905683414071feb2aaebf39fa0a44b0b2e5e
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-06 14:36:51 +0200

    Update save.php

--- pages/api/save.php
+++ pages/api/save.php
@@ -1,9 +1,16 @@
 <?php
-// Check if form is submitted and we have the fields we want.
+// server.php
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Retrieve the value of the 'text' parameter from the request body
+  
+  	echo 'Request processed successfully';
+
 	$file = __DIR__  . "data.txt";
 	$text =  $_POST["text"];
 	// This file will create a data.txt file and put whatever is in the POST field mytext into the text and put a new line on the end.
 	// The FILE_APPEND allows you to append text to the file. LOCK_EX prevents anyone else from writing to the file at the same time.
 	file_put_contents($file, $text . "\r\n", FILE_APPEND | LOCK_EX);
-
+}
 ?>
