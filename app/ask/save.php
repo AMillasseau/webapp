fae1fa5232971bc8facfdc8eae2abdc293f9e8f9
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-06 13:38:07 +0200

    Update and rename public/save.php to app/ask/save.php

--- app/ask/save.php
+++ app/ask/save.php
@@ -3,8 +3,10 @@
 if(isset($_POST["mytext"]))
 {
 	$file = "data.txt";
-	$text = $_POST["mytext"];
-
+	$name = $_POST["name"];
+	$contact = $_POST["contact"];
+	$message = $_POST["message"];
+	$text = "{$name} ({$contact}) : {$text}";
 	// This file will create a data.txt file and put whatever is in the POST field mytext into the text and put a new line on the end.
 	// The FILE_APPEND allows you to append text to the file. LOCK_EX prevents anyone else from writing to the file at the same time.
 	file_put_contents($file, $text . "\r\n", FILE_APPEND | LOCK_EX);
