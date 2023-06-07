b4c9a0417af283a6f3e38a74f5745a3ed72771d2
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 11:50:59 +0200

    Update send.js

--- public/scripts/send.js
+++ public/scripts/send.js
@@ -1,5 +1,5 @@
 funtion send(name, contact, message) {
-const query = 'INSERT INTO your_table (Name, Email, Text) VALUES ($1, $2, $3)';
+const query = 'INSERT INTO request (Name, Email, Text) VALUES ($1, $2, $3)';
 const values = [name, contact, message];
 
 pool.query(query, values, (error, result) => {
