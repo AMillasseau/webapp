e8c616f810f9061aab3e25e272cc4e6749e9dc9b
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 11:28:06 +0200

    Update page.js

--- app/api/test/page.js
+++ app/api/test/page.js
@@ -1,61 +1,11 @@
-<?php
-// Retrieve the request data
-$name = $_POST['name'];
-$email = $_POST['email'];
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { text } = req.body;
 
-// Perform any necessary operations or validations
+    // Do something with the received data, e.g., save it to a database
 
-// Prepare the response data
-$response = [
-    'message' => 'Received the following data:',
-    'name' => $name,
-    'email' => $email
-];
-
-// Set the response headers
-header('Content-Type: application/json');
-
-// Send the response as JSON
-echo json_encode($response);
-?>
-
-import Image from 'next/image'
-import Link from 'next/link'
-
-import style from 'app/page.module.css'
-
-export default function Home() {
-  return (
-    <main className={style.main}>
-  
-      <div className={style.head}>
-        <div className={style.notwide}>
-          <p> Website of Arena </p>
-          <div className={style.logo}>
-            <Image 
-              src="/logo.png"
-              width={100}
-              height={100}
-              alt="Arena's logo"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className={style.center}>
-        
-        
-        
-      </div>
-
-      <div className={style.tail}>
-        <div className={style.notwide}>
-          <a className={style.a} href="/credits"> Credits </a>
-          <p> • </p>
-          <a className={style.a} href="/ask"> Ask us </a>
-        </div>
-      </div>
-    
-      </main>
-    )
+    res.status(200).json({ message: 'Data received successfully' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
 }
