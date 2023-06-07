50a61ec7df88e4dc2e182cdaa856a236e82e88dc
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 11:12:48 +0200

    Update page.js

--- app/api/test/page.js
+++ app/api/test/page.js
@@ -18,3 +18,44 @@ header('Content-Type: application/json');
 // Send the response as JSON
 echo json_encode($response);
 ?>
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import style from 'app/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+  
+      <div className={style.head}>
+        <div className={style.notwide}>
+          <p> Website of Arena </p>
+          <div className={style.logo}>
+            <Image 
+              src="/logo.png"
+              width={100}
+              height={100}
+              alt="Arena's logo"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={style.center}>
+        
+        
+        
+      </div>
+
+      <div className={style.tail}>
+        <div className={style.notwide}>
+          <a className={style.a} href="/credits"> Credits </a>
+          <p> • </p>
+          <a className={style.a} href="/ask"> Ask us </a>
+        </div>
+      </div>
+    
+      </main>
+    )
+}
