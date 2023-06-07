25eb585901700ad9d3f67c1a618958d6e1069a2d
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 11:48:02 +0200

    Update page.js

--- app/ask/page.js
+++ app/ask/page.js
@@ -41,7 +41,7 @@ export default function Home() {
           <input className={style.ipt} id="name" name="name" type="text" placeholder="Name" autocomplete="off" required/>
           <input className={style.ipt} id="contact" name="contact" type="email" placeholder="Email contact"/>
           <textarea className={style.txta} id="message" name="text" placeholder="Your message" autocomplete="off" rows="12" required></textarea>
-          <button className={style.btn} id="btn" onclick="send()"> Submit </button>
+          <button className={style.btn} id="btn" onclick="send(document.getElementById('name').value,document.getElementById('contact').value,document.getElementById('message').value)"> Submit </button>
           </form>
       </div>
 
