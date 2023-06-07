6eb5e6eb88986b57d58e33ef876d96f3522ab3f8
AMillasseau <102949390+AMillasseau@users.noreply.github.com>
2023-06-07 11:46:16 +0200

    Update send.js

--- public/scripts/send.js
+++ public/scripts/send.js
@@ -1,19 +1,10 @@
-function send() {
-            const url ="https://webapp-amillasseau.vercel.app/api/save"
-            const name = document.getElementById("name").value;
-            const contact = document.getElementById("contact").value;
-            const message = document.getElementById("message").value;
-            const text = name + " ("+contact+") : "+message
-            makeRequest(url, text);
-          };
-  function makeRequest(url, text) {
-  // …
+funtion send(name, contact, message) {
+const query = 'INSERT INTO your_table (Name, Email, Text) VALUES ($1, $2, $3)';
+const values = [name, contact, message];
 
-  httpRequest.onreadystatechange = alertContents;
-  httpRequest.open("POST", url);
-  httpRequest.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded"
-  );
-  httpRequest.send(`text=${encodeURIComponent(text)}`);
-};
+pool.query(query, values, (error, result) => {
+  if (error) {
+    throw error;
+  }
+  console.log('Data inserted successfully');
+});}
